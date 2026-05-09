ec1fa558a7ec3623d22398e48c808948c4dbfa26
fazza30 <fazza.mufti@gmail.com>
2026-05-09 15:50:54 +0700

    Revert "fix thumbnail url"
    This reverts commit 42532e6b4416d095552ca98f76e43f65f08a825f.

--- backend/src/models/Movie.ts
+++ backend/src/models/Movie.ts
@@ -1,161 +1,81 @@
 import mongoose from "mongoose";
-
 import { getAssetUrl } from "../utils/helper";
-
 import Genre from "./Genre";
 import Theater from "./Theater";
 
-/**
- * ==================================================
- * MOVIE SCHEMA
- * ==================================================
- */
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Genre"
+    },
+    theaters: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Theater"
+        },
+    ],
+    description: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true
+    },
+    price: Number,
+    available: Boolean,
+    bonus: String
+}, {
+    virtuals: {
+        thumbnailUrl: {
+	get() {
+		return `${getAssetUrl()}/uploads/thumbnails/${this.thumbnail}`;
+	}
+}
+    },
+    toJSON: {
+        virtuals: true
+    }
+});
 
-const movieSchema =
-	new mongoose.Schema(
-		{
-			title: {
-				type: String,
-				required: true,
-			},
+movieSchema.post('save', async (doc) => {
+    if (doc) {
+        await Genre.findByIdAndUpdate(doc.genre, {
+            $push: {
+                movies: doc._id
+            }
+        })
+    }
 
-			genre: {
-				type: mongoose.Schema.Types.ObjectId,
-				ref: "Genre",
-			},
+    for (const theater of doc.theaters) {
+        await Theater.findByIdAndUpdate(theater._id, {
+            $push: {
+                movies: theater._id
+            }
+        })
+    }
+})
 
-			theaters: [
-				{
-					type: mongoose.Schema.Types.ObjectId,
-					ref: "Theater",
-				},
-			],
+movieSchema.post('deleteOne', async (doc) => {
+    if (doc) {
+        await Genre.findByIdAndUpdate(doc.genre, {
+            $pull: {
+                movies: doc._id
+            }
+        })
+    }
 
-			description: {
-				type: String,
-				required: true,
-			},
+    for (const theater of doc.theaters) {
+        await Theater.findByIdAndUpdate(theater._id, {
+            $pull: {
+                movies: theater._id
+            }
+        })
+    }
+})
 
-			thumbnail: {
-				type: String,
-				required: true,
-			},
-
-			price: Number,
-
-			available: Boolean,
-
-			bonus: String,
-		},
-		{
-			toJSON: {
-				virtuals: true,
-			},
-
-			toObject: {
-				virtuals: true,
-			},
-		},
-	);
-
-/**
- * ==================================================
- * VIRTUAL THUMBNAIL URL
- * ==================================================
- */
-
-movieSchema
-	.virtual(
-		"thumbnailUrl",
-	)
-	.get(function () {
-		return `${getAssetUrl(
-			"thumbnails",
-		)}${this.thumbnail}`;
-	});
-
-/**
- * ==================================================
- * AFTER SAVE
- * ==================================================
- */
-
-movieSchema.post(
-	"save",
-	async function (
-		doc,
-	) {
-		if (doc) {
-			await Genre.findByIdAndUpdate(
-				doc.genre,
-				{
-					$push: {
-						movies:
-							doc._id,
-					},
-				},
-			);
-		}
-
-		for (const theater of doc.theaters) {
-			await Theater.findByIdAndUpdate(
-				theater,
-				{
-					$push: {
-						movies:
-							doc._id,
-					},
-				},
-			);
-		}
-	},
-);
-
-/**
- * ==================================================
- * AFTER DELETE
- * ==================================================
- */
-
-movieSchema.post(
-	"deleteOne",
-	async function (
-		doc: any,
-	) {
-		if (doc) {
-			await Genre.findByIdAndUpdate(
-				doc.genre,
-				{
-					$pull: {
-						movies:
-							doc._id,
-					},
-				},
-			);
-		}
-
-		for (const theater of doc.theaters) {
-			await Theater.findByIdAndUpdate(
-				theater,
-				{
-					$pull: {
-						movies:
-							doc._id,
-					},
-				},
-			);
-		}
-	},
-);
-
-/**
- * ==================================================
- * EXPORT
- * ==================================================
- */
-
-export default mongoose.model(
-	"Movie",
-	movieSchema,
-	"movies",
-);
+export default mongoose.model('Movie', movieSchema, 'movies');
