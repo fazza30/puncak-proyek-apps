dcef3b0c40761d0e6df3b6c95e3a137649959d30
fazza30 <fazza.mufti@gmail.com>
2026-05-09 10:57:26 +0700

    fix vercel rewrite

--- frontend/src/services/genre/genre.service.ts
+++ frontend/src/services/genre/genre.service.ts
@@ -10,16 +10,16 @@ export const genreSchema = z.object({
 export type GenreValues = z.infer<typeof genreSchema>;
 
 export const getGenres = (): Promise<BaseResponse<Genre[]>> =>
-	privateInstance.get("/admin/genres").then((res) => res.data);
+	privateInstance.get("/api/admin/genres").then((res) => res.data);
 
 export const createGenre = (data: GenreValues) =>
-	privateInstance.post("/admin/genres", data).then((res) => res.data);
+	privateInstance.post("/api/admin/genres", data).then((res) => res.data);
 
 export const getDetailGenre = (id: string): Promise<BaseResponse<Genre>> =>
-	privateInstance.get(`/admin/genres/${id}`).then((res) => res.data);
+	privateInstance.get(`/api/admin/genres/${id}`).then((res) => res.data);
 
 export const updateGenre = (data: GenreValues, id: string) =>
-	privateInstance.put(`/admin/genres/${id}`, data).then((res) => res.data);
+	privateInstance.put(`/api/admin/genres/${id}`, data).then((res) => res.data);
 
 export const deleteGenre = (id: string) =>
-	privateInstance.delete(`/admin/genres/${id}`).then((res) => res.data);
+	privateInstance.delete(`/api/admin/genres/${id}`).then((res) => res.data);
