da8f3136092ff488d1ec8c4ee9173470eade2f22
fazza30 <fazza.mufti@gmail.com>
2026-05-19 12:02:43 +0700

    minor updates pt.5

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -52,7 +52,7 @@ export default function Header() {
 				<SheetContent side="left" className="flex flex-col">
 					<nav className="grid gap-2 text-lg font-medium">
 						<Link
-							to="#"
+							to="/admin/dashboard"
 							className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
 						>
 							<Home className="h-4 w-4" />
